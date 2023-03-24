8e6b3979a726cc5bc32bfd060e753c3d2a45b9da
Michael Williams <michael.williams@enspiral.com>
2023-03-24 17:06:20 +1300

    kiss3d viewer

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
 use anyhow::Error;
 use deno_core::resolve_path;
-use egui::RawInput;
 use fj_operations::shape_processor::ShapeProcessor;
-use fj_viewer::{GuiState, StatusReport, Viewer};
 use futures_lite::future;
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-};
 
 mod engine;
 mod loader;
-mod window;
+mod viewer;
 
 use crate::engine::Engine;
-use crate::window::Window;
+use crate::viewer::Viewer;
 
 fn main() -> Result<(), Error> {
-    let event_loop = EventLoop::new();
-    let window = Window::new(&event_loop)?;
-    let mut viewer = future::block_on(Viewer::new(&window))?;
-
     let mut engine = Engine::new();
     let current_dir = std::env::current_dir().expect("Unable to get CWD");
     let sketch_path = resolve_path("./sketches/demo.ts", &current_dir)?;
@@ -31,27 +21,11 @@ fn main() -> Result<(), Error> {
     let shape_processor = ShapeProcessor { tolerance: None };
     let processed_shape = shape_processor.process(&shape)?;
 
-    viewer.handle_shape_update(processed_shape);
+    let mut viewer = Viewer::new(processed_shape)?;
 
-    let pixels_per_point = 1_f32;
-    let egui_input = RawInput {
-        ..Default::default()
-    };
-    let gui_state = GuiState {
-        status: &StatusReport::new(),
-        model_available: true,
-    };
-    viewer.draw(pixels_per_point, egui_input, gui_state);
+    while viewer.render() {
+        viewer.step()
+    }
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
+    Ok(())
 }
---
+++ src/viewer.rs
@@ -0,0 +1,66 @@
+use fj_interop::processed_shape::ProcessedShape;
+use kiss3d::{
+    light::Light,
+    nalgebra::{Point3, UnitQuaternion, Vector3},
+    ncollide3d::procedural::TriMesh,
+    resource::Mesh,
+    scene::SceneNode,
+    window::Window,
+};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use anyhow::Error;
+
+pub struct Viewer {
+    window: Window,
+    shape_node: SceneNode,
+    rot: UnitQuaternion<f32>,
+}
+
+impl Viewer {
+    pub fn new(shape: ProcessedShape) -> Result<Self, Error> {
+        let mut window = Window::new("Sandkit");
+
+        let shape_mesh = shape_to_mesh(shape);
+        let mut shape_node = window.add_mesh(shape_mesh, Vector3::new(0.02, 0.02, 0.02));
+        shape_node.set_color(1.0, 0., 0.);
+
+        window.set_light(Light::StickToCamera);
+
+        let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+
+        Ok(Self {
+            window,
+            shape_node,
+            rot,
+        })
+    }
+
+    pub fn render(&mut self) -> bool {
+        self.window.render()
+    }
+
+    pub fn step(&mut self) {
+        self.shape_node.prepend_to_local_rotation(&self.rot)
+    }
+}
+
+fn shape_to_mesh(shape: ProcessedShape) -> Rc<RefCell<Mesh>> {
+    let mut coords = Vec::<Point3<f32>>::new();
+    let mut faces = Vec::<Point3<u16>>::new();
+
+    shape.mesh.triangles().for_each(|triangle| {
+        let mut face_indices = Vec::<u16>::new();
+        triangle.inner.points().into_iter().for_each(|point| {
+            let coord = Point3::new(point.x.into_f32(), point.y.into_f32(), point.z.into_f32());
+            coords.push(coord);
+            face_indices.push(coords.len() as u16 - 1);
+        });
+        let face = Point3::new(face_indices[0], face_indices[1], face_indices[2]);
+        faces.push(face);
+    });
+
+    let mesh = Mesh::new(coords, faces, None, None, false);
+    Rc::new(RefCell::new(mesh))
+}
